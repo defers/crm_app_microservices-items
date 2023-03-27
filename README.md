00f9b6349744e31c3d9085ee91779f22f37a7cc9
Алексей <defers86@gmail.com>
2023-03-27 22:34:17 +0300

    27032023

---
+++ README.md
@@ -0,0 +1,9 @@
+### CRM Items microservice
+
+##### reactive stack:
+1. Spring framework (without boot)
+2. Spring Webflux + Netty reactor
+3. Spring data reactive Mongo + MongoDB
+4. SOAP web service
+5. RabbitMQ
+
--- src/main/java/com/defers/crm/items/handler/ItemHandler.java
+++ src/main/java/com/defers/crm/items/handler/ItemHandler.java
@@ -1,14 +1,17 @@
 package com.defers.crm.items.handler;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 @Component
 public class ItemHandler {
     public Mono<ServerResponse> findAll(ServerRequest request) {
+        log.info("find all method invoked...");
         return ServerResponse.ok()
                 .body(BodyInserters.fromValue("test yo!"));
     }
